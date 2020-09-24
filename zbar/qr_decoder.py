2b01e262a3f5fec5e8511ee8b56a06f42bb814c3
ckswjd99 <ckswjd99@snu.ac.kr>
2020-09-24 23:36:29 +0900

    QR Code Encoder, Decoder Developed

--- zbar/qr_decoder.py
+++ zbar/qr_decoder.py
@@ -1,10 +1,14 @@
-#pip install opencv-python
+#pip install pyzbar opencv-python
+import copy
 import pyzbar.pyzbar as pyzbar
-import cv2
+import matplotlib.pylab as plt
+from PIL import Image
+import PIL.ImageOps
 
-def qr_decoder(img):
-    gray = gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    cv2.imshow('gray', gray)
+def qr_reader(img):
+    gray = img.convert('LA')
+    plt.imshow(gray)
+    gray.save("decoding.png")
     decoded = pyzbar.decode(gray)
     print(decoded)
 
@@ -16,7 +20,36 @@ def qr_decoder(img):
         data.append(d.data.decode('utf-8'))
     return data
 
-test_img = cv2.imread('result.png')
-test_img_reverse = cv2.bitwise_not(test_img)
+def qr_decoder(img, enc):
+    new_img = copy.deepcopy(img)
+    if enc == "":
+        pass
+    elif enc == "RED_SHUFFLE" or enc == "RED_BSHUFFLE":
+        for i in range(img.size[0]):
+            for j in range(img.size[1]):
+                r, g, b = new_img.getpixel((i, j))
+                new_img.putpixel((i,j), (r, r, r))
+        new_img = PIL.ImageOps.invert(new_img)
+    elif enc == "GREEN_SHUFFLE" or enc == "GREEN_BSHUFFLE":
+        for i in range(img.size[0]):
+            for j in range(img.size[1]):
+                r, g, b = new_img.getpixel((i, j))
+                new_img.putpixel((i,j), (g, g, g))
+        new_img = PIL.ImageOps.invert(new_img)
+        pass
+    elif enc == "BLUE_SHUFFLE" or enc == "BLUE_BSHUFFLE":
+        for i in range(img.size[0]):
+            for j in range(img.size[1]):
+                r, g, b = new_img.getpixel((i, j))
+                new_img.putpixel((i,j), (b, b, b))
+        new_img = PIL.ImageOps.invert(new_img)
+        pass
 
-print(qr_decoder(test_img_reverse))
+    new_img.save("decoding.png")
+    return new_img
+
+
+test_img = Image.open('result.png')
+test_img_reverse = PIL.ImageOps.invert(test_img)
+
+#qr_reader(qr_decoder(test_img, "RED_BSHUFFLE"))
--- zbar/qr_gen.py
+++ zbar/qr_gen.py
@@ -4,30 +4,66 @@ import matplotlib.pylab as plt
 
 def qr_gen(text):
     img = qrcode.make(text)
-    print(img.size)
+    img.save("original.png")
     return img
 
 def qr_encrypt(img, enc):
     new_img = img.convert('RGB')
-    
-    for i in range(new_img.size[0]):
-        for j in range(new_img.size[1]):
-            r, g, b = new_img.getpixel((i, j))
-            if enc == "RED":    # RED will be key color
+
+    if enc == "RED":
+        for i in range(new_img.size[0]):
+            for j in range(new_img.size[1]):
+                r, g, b = new_img.getpixel((i, j))
                 if (r, g, b) == (0, 0, 0):
                     new_img.putpixel((i,j), (255, 0, 0))
-                else:
-                    ran_g = random.randint(0,255)
-                    ran_b = random.randint(0,255)
-                    new_img.putpixel((i,j), (0, ran_g, ran_b))
-            elif enc == "RED_SHUFFLE":    # RED will be key color
+    elif enc == "RED_SHUFFLE":
+        for i in range(new_img.size[0]):
+            for j in range(new_img.size[1]):
+                r, g, b = new_img.getpixel((i, j))
                 ran_g = random.randint(0,255)
                 ran_b = random.randint(0,255)
                 if (r, g, b) == (0, 0, 0):
                     new_img.putpixel((i,j), (255, ran_g, ran_b))
                 else:
-                    
+                    ran_g = random.randint(0,255)
+                    ran_b = random.randint(0,255)
                     new_img.putpixel((i,j), (0, ran_g, ran_b))
+    elif enc == "RED_BSHUFFLE":
+        for i in range(round(new_img.size[0]/10)):
+            for j in range(round(new_img.size[1]/10)):
+                r, g, b = new_img.getpixel((i*10, j*10))
+                ran_g = random.randint(0,255)
+                ran_b = random.randint(0,255)
+                if (r, g, b) == (0, 0, 0):
+                    for k in range(100):
+                        new_img.putpixel((i*10+int(k/10),j*10+k%10), (255, ran_g, ran_b))
+                else:
+                    for k in range(100):
+                        new_img.putpixel((i*10+int(k/10),j*10+k%10), (0, int(ran_g), int(ran_b)))
+    elif enc == "GREEN_BSHUFFLE":
+        for i in range(round(new_img.size[0]/10)):
+            for j in range(round(new_img.size[1]/10)):
+                r, g, b = new_img.getpixel((i*10, j*10))
+                ran_r = random.randint(0,255)
+                ran_b = random.randint(0,255)
+                if (r, g, b) == (0, 0, 0):
+                    for k in range(100):
+                        new_img.putpixel((i*10+int(k/10),j*10+k%10), (ran_r, 255, ran_b))
+                else:
+                    for k in range(100):
+                        new_img.putpixel((i*10+int(k/10),j*10+k%10), (int(ran_r), 0, int(ran_b)))
+    elif enc == "BLUE_BSHUFFLE":
+        for i in range(round(new_img.size[0]/10)):
+            for j in range(round(new_img.size[1]/10)):
+                r, g, b = new_img.getpixel((i*10, j*10))
+                ran_r = random.randint(0,255)
+                ran_g = random.randint(0,255)
+                if (r, g, b) == (0, 0, 0):
+                    for k in range(100):
+                        new_img.putpixel((i*10+int(k/10),j*10+k%10), (ran_r, ran_g, 255))
+                else:
+                    for k in range(100):
+                        new_img.putpixel((i*10+int(k/10),j*10+k%10), (int(ran_r), int(ran_g), 0))
 
 
     plt.imshow(new_img)
@@ -37,4 +73,4 @@ def qr_encrypt(img, enc):
 
     return new_img
 
-
+qr = qr_gen("hello")
