f95c88b2747eaea5c81aefb9b5f6ada5cb86847b
ckswjd99 <ckswjd99@snu.ac.kr>
2020-10-02 19:21:19 +0900

    Key Vector Encoding&Decoding

--- zbar/qr_decoder.py
+++ zbar/qr_decoder.py
@@ -1,9 +1,41 @@
-#pip install pyzbar opencv-python
+#pip install pyzbar pillow matplotlib numpy opencv-python
 import copy
 import pyzbar.pyzbar as pyzbar
 import matplotlib.pylab as plt
 from PIL import Image
+from PIL import ImageFilter
 import PIL.ImageOps
+import numpy as np
+
+key_vector = np.array([1,0,0])
+
+def onbasis(seed_vector):
+    e1 = np.array([1,0,0])
+    e2 = np.array([0,1,0])
+    e3 = np.array([0,0,1])
+
+    u1 = np.array(seed_vector)
+    u1 = u1 / np.linalg.norm(u1)
+
+    u2 = e2 - np.dot(e2, u1) / np.dot(u1, u1) * u1
+    u2 = u2 / np.linalg.norm(u2)
+
+    u3 = e3 - np.dot(e3, u1) / np.dot(u1, u1) * u1 - np.dot(e3, u2) / np.dot(u2, u2) * u2
+    u3 = u3 / np.linalg.norm(u3)
+
+    return [u1, u2, u3]
+
+def transform_vector(vector, onbasis):
+    x1 = np.dot(vector, onbasis[0])
+    x2 = np.dot(vector, onbasis[1])
+    x3 = np.dot(vector, onbasis[2])
+    return (x1, x2, x3)
+
+def invtransform_vector(vector, onbasis):
+    x1 = vector[0]*onbasis[0][0] + vector[1]*onbasis[1][0] + vector[2]*onbasis[2][0]
+    x2 = vector[0]*onbasis[0][1] + vector[1]*onbasis[1][1] + vector[2]*onbasis[2][1]
+    x3 = vector[0]*onbasis[0][2] + vector[1]*onbasis[1][2] + vector[2]*onbasis[2][2]
+    return (x1, x2, x3)
 
 def qr_reader(img):
     gray = img.convert('LA')
@@ -22,6 +54,8 @@ def qr_reader(img):
 
 def qr_decoder(img, enc):
     new_img = copy.deepcopy(img)
+    GaussianBlur = ImageFilter.GaussianBlur(3)
+    new_img = new_img.filter(GaussianBlur)
     if enc == "":
         pass
     elif enc == "RED_SHUFFLE" or enc == "RED_BSHUFFLE":
@@ -42,6 +76,26 @@ def qr_decoder(img, enc):
             for j in range(img.size[1]):
                 r, g, b = new_img.getpixel((i, j))
                 new_img.putpixel((i,j), (b, b, b))
+        new_img = PIL.ImageOps.invert(new_img)
+        pass
+    elif enc == "YELLOW_SHUFFLE" or enc == "YELLOW_BSHUFFLE":
+        norm = np.linalg.norm(np.array([1,1]))
+        basis = [np.array([1,1,0])/norm, np.array([0,1,1])/norm, np.array([1,0,1])/norm]
+        for i in range(img.size[0]):
+            for j in range(img.size[1]):
+                pixel = np.array(new_img.getpixel((i, j)))
+                ycm = tuple(np.array(transform_vector(tuple(pixel), basis)).astype(int))
+                new_img.putpixel((i,j), (ycm[0], ycm[0], ycm[0]))
+        new_img = PIL.ImageOps.invert(new_img)
+        pass
+    elif enc == "VECTOR_BSHUFFLE":
+        norm = np.linalg.norm(np.array([1,1]))
+        basis = onbasis(key_vector)
+        for i in range(img.size[0]):
+            for j in range(img.size[1]):
+                pixel = np.array(new_img.getpixel((i, j)))
+                ycm = tuple(np.array(transform_vector(tuple(pixel), basis)).astype(int))
+                new_img.putpixel((i,j), (ycm[0], ycm[0], ycm[0]))
         new_img = PIL.ImageOps.invert(new_img)
         pass
 
--- zbar/qr_gen.py
+++ zbar/qr_gen.py
@@ -1,6 +1,51 @@
-# pip install qrcode pillow matplotlib
-import qrcode, numpy, random
+# pip install qrcode pillow matplotlib numpy
+import qrcode, random
+import numpy as np
 import matplotlib.pylab as plt
+
+def random_onbasis():
+    e1 = np.array([1,0,0])
+    e2 = np.array([0,1,0])
+    e3 = np.array([0,0,1])
+    
+    u1 = np.array([random.randint(-255,255), random.randint(-255,255), random.randint(-255,255)])
+    u1 = u1 / np.linalg.norm(u1)
+
+    u2 = e2 - np.dot(e2, u1) / np.dot(u1, u1) * u1
+    u2 = u2 / np.linalg.norm(u2)
+
+    u3 = e3 - np.dot(e3, u1) / np.dot(u1, u1) * u1 - np.dot(e3, u2) / np.dot(u2, u2) * u2
+    u3 = u3 / np.linalg.norm(u3)
+
+    return [u1, u2, u3]
+
+def onbasis(seed_vector):
+    e1 = np.array([1,0,0])
+    e2 = np.array([0,1,0])
+    e3 = np.array([0,0,1])
+
+    u1 = np.array(seed_vector)
+    u1 = u1 / np.linalg.norm(u1)
+
+    u2 = e2 - np.dot(e2, u1) / np.dot(u1, u1) * u1
+    u2 = u2 / np.linalg.norm(u2)
+
+    u3 = e3 - np.dot(e3, u1) / np.dot(u1, u1) * u1 - np.dot(e3, u2) / np.dot(u2, u2) * u2
+    u3 = u3 / np.linalg.norm(u3)
+
+    return [u1, u2, u3]
+
+def transform_vector(vector, onbasis):
+    x1 = np.dot(vector, onbasis[0])
+    x2 = np.dot(vector, onbasis[1])
+    x3 = np.dot(vector, onbasis[2])
+    return (x1, x2, x3)
+
+def invtransform_vector(vector, onbasis):
+    x1 = vector[0]*onbasis[0][0] + vector[1]*onbasis[1][0] + vector[2]*onbasis[2][0]
+    x2 = vector[0]*onbasis[0][1] + vector[1]*onbasis[1][1] + vector[2]*onbasis[2][1]
+    x3 = vector[0]*onbasis[0][2] + vector[1]*onbasis[1][2] + vector[2]*onbasis[2][2]
+    return (x1, x2, x3)
 
 def qr_gen(text):
     img = qrcode.make(text)
@@ -64,6 +109,49 @@ def qr_encrypt(img, enc):
                 else:
                     for k in range(100):
                         new_img.putpixel((i*10+int(k/10),j*10+k%10), (int(ran_r), int(ran_g), 0))
+    elif enc == "YELLOW_BSHUFFLE":
+        norm = np.linalg.norm(np.array([1,1]))
+        basis = [np.array([1,1,0])/norm, np.array([0,1,1])/norm, np.array([1,0,1])/norm]
+        for i in range(round(new_img.size[0]/10)):
+            for j in range(round(new_img.size[1]/10)):
+                r, g, b = new_img.getpixel((i*10, j*10))
+                if (r, g, b) == (0, 0, 0):
+                    new_pixel = np.array(invtransform_vector((255, random.randint(0,255), random.randint(0,255)), (basis)))
+                    new_pixel = new_pixel / np.max(new_pixel) * 255
+                    new_pixel = new_pixel.astype(int)
+                    
+                    for k in range(100):
+                        new_img.putpixel((i*10+int(k/10),j*10+k%10), tuple(new_pixel))
+                else:
+                    new_pixel = np.array(invtransform_vector((0, random.randint(0,255), random.randint(0,255)), (basis)))
+                    new_pixel = new_pixel / np.max(new_pixel) * 255
+                    new_pixel = new_pixel.astype(int)
+                    
+                    for k in range(100):
+                        new_img.putpixel((i*10+int(k/10),j*10+k%10), tuple(new_pixel))
+    elif enc == "RANDOM_BSHUFFLE":
+        norm = np.linalg.norm(np.array([1,1]))
+        basis = random_onbasis()
+        print(basis)
+        for i in range(round(new_img.size[0]/10)):
+            for j in range(round(new_img.size[1]/10)):
+                r, g, b = new_img.getpixel((i*10, j*10))
+                if (r, g, b) == (0, 0, 0):
+                    new_pixel = np.array(invtransform_vector((255, random.randint(0,255), random.randint(0,255)), (basis)))
+                    new_pixel = new_pixel / np.max(new_pixel) * 255
+                    new_pixel = new_pixel / 2 + 127
+                    new_pixel = new_pixel.astype(int)
+                    
+                    for k in range(100):
+                        new_img.putpixel((i*10+int(k/10),j*10+k%10), tuple(new_pixel))
+                else:
+                    new_pixel = np.array(invtransform_vector((0, random.randint(0,255), random.randint(0,255)), (basis)))
+                    new_pixel = new_pixel / np.max(new_pixel) * 255
+                    new_pixel = new_pixel / 2 + 127
+                    new_pixel = new_pixel.astype(int)
+
+                    for k in range(100):
+                        new_img.putpixel((i*10+int(k/10),j*10+k%10), tuple(new_pixel))
 
 
     plt.imshow(new_img)
